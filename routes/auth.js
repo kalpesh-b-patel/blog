529ed743c59d38c542447120cf45c4a42256202c
Kalpesh Patel <kalpeshpatel.it@gmail.com>
2020-10-11 23:17:47 -0500

    Initial version

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 const {
   createUser,
   getUserByEmailAndPassword,
@@ -12,5 +14,13 @@ router.route('/login')
 
 router.route('/verify')
   .get(verifyEmail);
+
+router.post('/file', upload.array('file'), (req, res) => {
+  const files = req.files;
+  const test = req.body.test;
+  console.log(files);
+  console.log(test);
+  res.status(200).json({});
+});
 
 module.exports = router;
